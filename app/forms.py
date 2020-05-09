21acc267a75e9f4a0e9c81c83a1577ba17bff047
ErikBoesen <me@erikboesen.com>
2020-05-09 03:25:14 -0400

    Add form

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import ValidationError, DataRequired, Length, URL
+from app.models import User
+
+
+class BotForm(FlaskForm):
+    chrome_url  = StringField('Chrome Web Store URL')
+    firefox_url = StringField('Mozilla Add-ons URL')
+    safari_url  = StringField('Safari Extension URL')
+    opera_url   = StringField('Opera Extension URL')
+    edge_url    = StringField('MS Edge Extension URL')
+
+    fallback_url = StringField('Fallback URL', validators=[DataRequired()])
+
+    submit = SubmitField('Submit')
