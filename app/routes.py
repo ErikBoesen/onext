c5da64de14c9e0365c16ace3bfddb5cd64f5da78
ErikBoesen <me@erikboesen.com>
2020-05-09 03:33:42 -0400

    Do creation in root

--- app/routes.py
+++ app/routes.py
@@ -6,25 +6,9 @@ from app.models import Link
 import random
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           bots=bots.items)
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-def random_string():
-    return ''.join([random.choice(string.ascii_letters) for i in range(8)])
-
-
-@app.route('/create_bot', methods=['GET', 'POST'])
-@login_required
-def create_link():
-    form = BotForm()
+    form = LinkForm()
     if form.validate_on_submit():
         link = Link(id=form.id.data,
                     chrome_url=form.chrome_url.data,
@@ -36,7 +20,14 @@ def create_link():
         db.session.add(link)
         db.session.commit()
         flash('Created link ' + link.id + '!')
-        return redirect(url_for('edit_bot', slug=bot.slug))
-    return render_template('edit_bot.html',
-                           title='Create new bot',
-                           form=form)
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+def random_string():
+    return ''.join([random.choice(string.ascii_letters) for i in range(8)])
+
