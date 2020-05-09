f8f142317f2e7e3c876678d7156161aba27cb80c
ErikBoesen <me@erikboesen.com>
2020-05-09 03:25:04 -0400

    Add __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models, errors
