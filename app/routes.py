4dd134bba00151ea27f91b585f4e18499e11d6d3
ErikBoesen <me@erikboesen.com>
2020-05-09 03:24:36 -0400

    Start adding routes

---
+++ app/routes.py
@@ -0,0 +1,47 @@
+import requests
+from flask import render_template, flash, redirect, url_for, request, abort, make_response
+from app import app, db
+from app.forms import LinkForm
+from app.models import Link
+import random
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           bots=bots.items)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+def random_string():
+    return ''.join([random.choice(string.ascii_letters) for i in range(8)])
+
+
+@app.route('/create_bot', methods=['GET', 'POST'])
+@login_required
+def create_link():
+    form = BotForm()
+    if form.validate_on_submit():
+        link = Link(id=,
+                    chrome_url=form.chrome_url.data,
+                    firefox_url=form.firefox_url.data,
+                    safari_url=form.safari_url.data,
+                    opera_url=form.opera_url.data,
+                    edge_url=form.edge_url.data,
+
+
+
+                  )
+        bot.reset_token()
+        bot.owner = current_user
+        db.session.add(bot)
+        db.session.commit()
+        flash('Successfully created bot ' + bot.name + '!')
+        return redirect(url_for('edit_bot', slug=bot.slug))
+    return render_template('edit_bot.html',
+                           title='Create new bot',
+                           form=form)
