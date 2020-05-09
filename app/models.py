7f544d27ca0c6b4804a41643bbe64e7be5598fe8
ErikBoesen <me@erikboesen.com>
2020-05-09 03:26:13 -0400

    Add Link model

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import app, db, login
+import os
+import binascii
+
+
+class Link(db.Model):
+    id = db.Column(db.String, primary_key=True)
+
+    chrome_url = db.Column(db.String)
+    firefox_url = db.Column(db.String)
+    safari_url = db.Column(db.String)
+    opera_url = db.Column(db.String)
+    edge_url = db.Column(db.String)
+
+    fallback_url = db.Column(db.String)
