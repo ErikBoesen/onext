7f4d0db03fe4aee44d3d2bebdedc633719e96843
ErikBoesen <me@erikboesen.com>
2022-04-12 12:59:28 -0400

    Add QR code

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,24 @@
-oninput = function(e) {
+var p = {
+    form: document.getElementsByClassName('form')[0],
+    id: document.getElementById('id'),
+    submit: document.querySelector('text[type="submit"]'),
+    copyContent: document.getElementById('copy-content'),
+    qr: document.getElementById('qr'),
+};
+
+addEventListener('input', function(e) {
     let target = e.target;
     if (target.value == '') {
         target.parentElement.className = 'entry';
     } else {
         target.parentElement.className = 'entry full';
     }
-};
+});
+
+function updateQR() {
+    p.qr.src = 'https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=onext.to/' + p.id.value;
+}
+updateQR();
+p.id.addEventListener('change', function() {
+    updateQR();
+});
